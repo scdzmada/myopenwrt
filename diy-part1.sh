699c7d52881577b6a0d89121297f5c468453f519
scdzmada <59276959+scdzmada@users.noreply.github.com>
2023-02-03 10:19:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #加入 DDNSTO到feeds配置文件里
-echo 'src-git nas https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
